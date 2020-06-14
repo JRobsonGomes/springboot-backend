fe5e6e9d399815dc34eb098e1ec89a127cbe58c5
Robson <robsong200@gmail.com>
2020-06-14 11:40:29 -0300

    Entidade Category

---
+++ src/main/java/com/example/cursospring/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.example.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cursospring.entities.Category;
+import com.example.cursospring.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
